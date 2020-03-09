53867ae0ac366ff80e2c311ae19ab674005d61e3
ha6icht <rolf.karlen@gmail.com>
2020-03-09 18:20:42 +0100

    Update TodoItems.js

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -10,7 +10,20 @@ class TodoItems extends Component {
         this.props.delete(key);
     }
     createTasks(item) {
-        return <li onClick={() => this.delete(item.key)} key={item.key}>{item.text}</li>
+        return (
+            <li key={item.key}>
+                <button onClick={() => this.delete(item.key)}>
+                    <svg width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                        <path fill="currentColor" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path>
+                    </svg>
+                </button>
+                <label>
+                    <input type="checkbox" /> 
+                     {item.text}
+                </label>
+            </li>   
+        );
     }
     
     render() {
