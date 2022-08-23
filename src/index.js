6afc2b3825fac0e0c532779e7852ef9cd75e0e1c
ha6icht <rolf.karlen@gmail.com>
2022-08-23 18:19:43 +0200

    Change link to App.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import TodoList from './TodoList';
-import * as serviceWorker from './serviceWorker';
+//import './index.css';
+//import TodoList from './TodoList';
+//import * as serviceWorker from './serviceWorker';
+import App from './App';
 
 var destination = document.querySelector("#container");
 
 ReactDOM.render(
     <div>
-        <TodoList />
+        <App />
     </div>,
     destination
 );
@@ -16,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
