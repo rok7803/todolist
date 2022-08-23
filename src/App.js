cceb44bf3ecefce2b9df82ad00da1323ef172304
ha6icht <rolf.karlen@gmail.com>
2022-08-23 18:42:42 +0200

    Change to class syntax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Board from './components/Board';
 import Card from './components/Card';
 import './main.css';
 
-function App(){
-	return (
-		<div className="App">
-			<main className="flexbox">
-				<Board id="board-1" className="board">
-					<Card id="card-1" className="card" draggable="true">
-						<p>Card one</p>
-					</Card>
-				</Board>
-				<Board id="board-2" className="board">
-					<Card id="card-2" className="card" draggable="true">
-						<p>Card two</p>
-					</Card>
-				</Board>
-			</main>
-		</div>
-	);
+class App extends Component {
+	render(){
+		return (
+			<div className="App">
+				<main className="flexbox">
+					<Board id="board-1" className="board">
+						<Card id="card-1" className="card" draggable="true">
+							<p>Card one</p>
+						</Card>
+					</Board>
+					<Board id="board-2" className="board">
+						<Card id="card-2" className="card" draggable="true">
+							<p>Card two</p>
+						</Card>
+					</Board>
+				</main>
+			</div>
+		);
+	}
 }
 export default App;
 	
