b5f4fd0b776fedf44021ad2a8b4d7fae6673a326
ha6icht <rolf.karlen@gmail.com>
2022-08-24 10:32:44 +0200

    Add this.state in constructor

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class Card extends Component {
     constructor(props) {
         super(props);
-		
+		this.state = {
+			elementToDrag: ''
+		}
 		this.dragStart = this.dragStart.bind(this);
 		this.dragOver = this.dragOver.bind(this);
 	}
