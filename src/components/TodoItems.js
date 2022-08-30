321b0d1492c145a3808fca45c2c3e3f179e59882
ha6icht <rolf.karlen@gmail.com>
2022-08-30 14:33:53 +0200

    First commit

---
+++ src/components/TodoItems.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import './TodoItems.css';
+ 
+class TodoItems extends Component {
+	
+	/*constructor(props){
+		super(props)
+		this.state = {
+			tasks: []
+		}
+		//this.addItemsToState = this.addItemsToState.bind(this);
+		//this.getItemsFromState = this.getItemsFromState.bind(this);
+	}*/
+
+    onDragStart = (ev, id) => {
+        console.log('dragstart:',id);
+		console.log(this.state);
+        ev.dataTransfer.setData("id", id);
+    }
+
+    onDragOver = (ev) => {
+        ev.preventDefault();
+    }
+	/**********Why is it working?********************/
+    onDrop = (ev, cat) => {
+		let id = ev.dataTransfer.getData("id");
+		console.log(id);
+		//console.log(task.name);
+		let tasks = this.props.entries.filter((task) => {
+			console.log(task.name);
+			if (task.name === id) {
+				console.log('INSIDE if');
+				task.category = cat;
+			}
+			return task;
+		});
+		console.log('INSIDE onDrop:',tasks);
+		//this.props.items(tasks);
+		/*this.setState=({
+		   ...this.state,
+		   tasks
+		});*/
+		/***********NOT RECOMMENDET!*******************/
+		this.forceUpdate();
+    }
+	
+    render() {
+        let tasks = {
+            wip: [],
+            complete: []
+        }
+		console.log('this.state:',this.state);
+		console.log(this.props.entries);
+				
+        this.props.entries.forEach ((t) => {
+            tasks[t.category].push(
+                <div id="above" key={t.name} 
+                    onDragStart = {(e) => this.onDragStart(e, t.name)}
+                    draggable
+                    className="draggable"
+                    style = {{backgroundColor: t.bgcolor}}
+                >
+                    {t.name}
+                </div>
+            );
+        });
+
+        return (
+            <div className="container-drag">
+                <h2 className="header">DRAG & DROP DEMO</h2>
+                <div id="wip" className="wip"
+                    onDragOver={(e)=>this.onDragOver(e)}
+                    onDrop={(e)=>{this.onDrop(e, "wip")}}>
+                    <span className="task-header">WIP</span>
+                    {tasks.wip}
+                </div>
+                <div id="completed" className="droppable" 
+                    onDragOver={(e)=>this.onDragOver(e)}
+                    onDrop={(e)=>this.onDrop(e, "complete")}>
+                     <span className="task-header">COMPLETED</span>
+                     {tasks.complete}
+                </div>
+
+
+            </div>
+        );
+    }
+}
+ 
+export default TodoItems;
