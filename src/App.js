f4886de1a69c79ae69d3cafb45f482f6177f2238
ha6icht <rolf.karlen@gmail.com>
2022-08-30 14:33:21 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoList from './TodoList'
+import './App.css'
+
+class App extends React.Component{
+	
+	render(){
+		return(
+			<TodoList />
+		)
+	}
+	
+	
+}
+export default App;
